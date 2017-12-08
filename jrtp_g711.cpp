02b1ea5eb3f961a11a8f0a8770757ea236a04bed
yqwung <1522943999@qq.com>
2017-12-08 18:40:07 +0800

    make-ready

--- jrtp_g711.cpp
+++ jrtp_g711.cpp
@@ -23,9 +23,9 @@ void checkerror(int rtperr)
 }
 
 
-void rtpPrintf(unsigned char *buf, unsigned int len)
+void rtpPrintf(uint8_t *buf, uint16_t len)
 {
-	unsigned int i=0;
+	uint16_t i=0;
 		
 	printf("RTP len=%d : \n", len);
 	
@@ -55,50 +55,50 @@ int main(void)
 	uint8_t destip[]={192, 168, 2, 105};
 
 	FILE *fd;
-	unsigned int read_len = 0;
-	unsigned char buf[1024] = {0};
+	uint16_t read_len = 0;
+	uint8_t buf[1024] = {0};
 
 
 	RTPUDPv4TransmissionParams transparams;
 	RTPSessionParams sessparams;
 
-    /* set g711a param */
-    sessparams.SetUsePredefinedSSRC(true);         // 设置使用预先定义的SSRC    
-    sessparams.SetOwnTimestampUnit(1.0/8000.0);    // 设置采样间隔 
-    sessparams.SetAcceptOwnPackets(true);          // 接收自己发送的数据包  
-    
-    transparams.SetPortbase(portbase);
+	/* set g711a param */
+	sessparams.SetUsePredefinedSSRC(true);         // 设置使用预先定义的SSRC    
+	sessparams.SetOwnTimestampUnit(1.0/8000.0);    // 设置采样间隔 
+	sessparams.SetAcceptOwnPackets(true);          // 接收自己发送的数据包  
+
+	transparams.SetPortbase(portbase);
 	status = sess.Create(sessparams,&transparams);
 	checkerror(status);
-	
+
 	RTPIPv4Address addr(destip,destport);
 	status = sess.AddDestination(addr);
 	checkerror(status);
 
-    sess.SetDefaultTimestampIncrement(320);        // 设置默认时间戳增加间隔  320
-    sess.SetDefaultPayloadType(8);                 // 设置默认负载类型 pcma的负载类型为8
-    sess.SetDefaultMark(true);                     // 设置默认标识
-	
-		
-    fd = fopen("./test.g711", "rb");               // g711a pcma
+	sess.SetDefaultTimestampIncrement(320);        // 设置默认时间戳增加间隔  320
+	sess.SetDefaultPayloadType(8);                 // 设置默认负载类型 pcma的负载类型为8
+	sess.SetDefaultMark(true);                     // 设置默认标识
+
+
+	fd = fopen("./test.g711", "rb");               // g711a pcma
 	if(!fd)
 	{
 		printf("error: can not open file !\n");
-		
+
 		return 0;
 	}
 	
 	while( !feof(fd) )    
 	{	
 		read_len = fread(buf, 1, 320, fd);         
-        //rtpPrintf(buf, read_len);
-		
+		//rtpPrintf(buf, read_len);
+
 		status = sess.SendPacket(buf, read_len, 8, true, 320);  
 		checkerror(status);
 
 		RTPTime::Wait(0.03);                     
 	} 	
 	fclose(fd);
-	
+
 	return 0;
 }
